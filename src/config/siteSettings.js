e39067d2d17b140b16a1c5774376b700f075f82d
lcschoolwebsite-ux <lcschoolwebsite@gmail.com>
2026-09-02 20:37:01 +0530

    fix(contact): update Deepak Hospital to exact coordinates 12.9263, 77.5773

--- src/config/siteSettings.js
+++ src/config/siteSettings.js
@@ -18,8 +18,8 @@ export const siteSettings = {
       name: 'Deepak Hospital',
       area: 'Jayanagar, Bangalore',
       address: 'Deepak Hospital, 33rd Cross Rd, 7th Block, Jayanagar, Bengaluru, Karnataka 560082',
-      mapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.5230543847545!2d77.58185347473823!3d12.925771815619775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1597abaf55eb%3A0xa61dbcf3bb6df6e1!2sDeepak%20Hospital!5e0!3m2!1sen!2sin!4v1725280000000!5m2!1sen!2sin',
-      mapsUrl: 'https://maps.google.com/?q=Deepak+Hospital+Jayanagar+Bangalore'
+      mapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.5230543847545!2d77.57734414071014!3d12.926343269529951!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1597abaf55eb%3A0xa61dbcf3bb6df6e1!2sDeepak%20Hospital!5e0!3m2!1sen!2sin!4v1725280000000!5m2!1sen!2sin',
+      mapsUrl: 'https://www.google.com/maps?q=12.926343269529951,77.57734414071014'
     },
     {
       name: 'Hemalatha Hospital',
