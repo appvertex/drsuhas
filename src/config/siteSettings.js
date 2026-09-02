7f8f622924d6ef66f5e444aeb23e20072ea7f0a7
lcschoolwebsite-ux <lcschoolwebsite@gmail.com>
2026-09-02 20:35:43 +0530

    fix(contact): update Hemalatha Hospital to exact coordinates 12.9535, 77.6167

--- src/config/siteSettings.js
+++ src/config/siteSettings.js
@@ -25,8 +25,8 @@ export const siteSettings = {
       name: 'Hemalatha Hospital',
       area: 'Neelasandra, Bangalore',
       address: 'Hemalatha Hospital, Neelasandra, Bengaluru, Karnataka 560047',
-      mapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.123456789012!2d77.61234567473823!3d12.955771815619775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae153b2a1f4f31%3A0xb12bcd0e45678901!2sHemalatha%20Hospital%2C%20Neelasandra!5e0!3m2!1sen!2sin!4v1725280000001!5m2!1sen!2sin',
-      mapsUrl: 'https://maps.google.com/?q=Hemalatha+Hospital+Neelasandra+Bangalore'
+      mapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.6123456789!2d77.61669501145074!3d12.953531471448763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae153b2a1f4f31%3A0xb12bcd0e45678901!2sHemalatha%20Hospital%2C%20Neelasandra!5e0!3m2!1sen!2sin!4v1725280000001!5m2!1sen!2sin',
+      mapsUrl: 'https://www.google.com/maps?q=12.953531471448763,77.61669501145074'
     }
   ],
   socials: {
